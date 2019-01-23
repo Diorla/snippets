8c0f0d6a9c9fdd447e4921d8c64b6de2f1137cd6
Ade Adeola <adedotster@gmail.com>
2019-01-24 01:53:50 +0200

    Added unique sort

---
+++ unique sort file.py
@@ -0,0 +1,34 @@
+def sort_file():
+    from tkinter.filedialog import askopenfilename
+
+    # open dialog
+    filename = askopenfilename()
+
+    # get the content of file
+    readfile = open(filename)
+
+    # generate array, str.split('\n')
+    arr = readfile.readlines()
+    readfile.close()
+
+    # remove duplicates
+    arr = list(set(arr))
+    
+    # sort the array
+    arr.sort()
+    
+    # update the file
+    writefile = open(filename, "w")
+    writefile.writelines(arr)
+    writefile.close()
+
+
+    # user feedback
+
+    print("file updated")
+    input("press enter to quit")
+
+
+
+if __name__ == "__main__":
+    sort_file()
